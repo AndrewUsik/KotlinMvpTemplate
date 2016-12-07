19e5320d2cbe9d5c6ae184e354162f4aaefb3d2d
Andrew <andrewusik1@gmail.com>
2016-12-07 16:35:15 +0200

    Small fix

--- app/src/main/kotlin/com/mobile/wezom/kotlinmvp/mvp/presenter/HomeFragmentPresenter.kt
+++ app/src/main/kotlin/com/mobile/wezom/kotlinmvp/mvp/presenter/HomeFragmentPresenter.kt
@@ -5,6 +5,7 @@ import com.arellomobile.mvp.InjectViewState
 import com.mobile.wezom.kotlinmvp.api.ApiManager
 import com.mobile.wezom.kotlinmvp.mvp.view.HomeFragmentView
 import de.greenrobot.event.EventBus
+import io.reactivex.disposables.Disposable
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -29,9 +30,10 @@ class HomeFragmentPresenter : BasePresenter<HomeFragmentView>() {
     }
 
     fun getUser() {
-        apiManager.getUser()
+        val disposable: Disposable = apiManager.getUser()
                 .subscribe({ list ->
                     viewState.setText(list.title as String)
                 }, { throwable -> Timber.e("get user request ", throwable) })
+        unsubscribeOnDestroy(disposable)
     }
 }
